51f841d260d59861fe3ee3e4e04a34d55c690114
Josh D <joj237@gmail.com>
2024-06-20 11:19:57 -0500

    Add longestPalindrome function to find the longest palindromic substring

---
+++ 0x01-dart-function/10-main.dart
@@ -0,0 +1,7 @@
+import '10-longest-palindrome.dart';
+
+void main() {
+  print(longestPalindrome("abcde")); // Should print "none"
+  print(longestPalindrome("azeghjhg")); // Should print "ghjhg"
+  print(longestPalindrome("aaabvf")); // Should print "aaa"
+}
