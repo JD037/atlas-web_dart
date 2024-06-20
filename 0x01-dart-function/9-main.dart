7e49d915cb2370cad6e7685dcd24fbd781d73ee1
Josh D <joj237@gmail.com>
2024-06-20 11:08:13 -0500

    Add isPalindrome function to check for palindromes

---
+++ 0x01-dart-function/9-main.dart
@@ -0,0 +1,7 @@
+import '9-palindrome.dart';
+
+void main() {
+  print(isPalindrome("aa")); // Should print false
+  print(isPalindrome("abcba")); // Should print true
+  print(isPalindrome("abcde")); // Should print false
+}
