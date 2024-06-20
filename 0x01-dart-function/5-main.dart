84ac3bf86b17d384139c4082d45d90a4a601f55e
Josh D <joj237@gmail.com>
2024-06-20 10:07:34 -0500

    Add calculateArea function to compute triangle area

---
+++ 0x01-dart-function/5-main.dart
@@ -0,0 +1,7 @@
+import '5-calculate-area.dart';
+
+void main() {
+  print(calculateArea(20, 7)); // Should print 70.0
+  print(calculateArea(10, 7)); // Should print 35.0
+  print(calculateArea(8.5, 3.7)); // Should print 15.73
+}
