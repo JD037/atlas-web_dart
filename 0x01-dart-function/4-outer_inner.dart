9774876411090797f9c599192a6ea19680dc9e02
Josh D <joj237@gmail.com>
2024-06-20 10:03:23 -0500

    Add outer and inner functions for agent greeting

---
+++ 0x01-dart-function/4-outer_inner.dart
@@ -0,0 +1,8 @@
+void outer(String name, String id) {
+  String inner() {
+    var parts = name.split(' ');
+    return 'Hello Agent ${parts[1][0]}.${parts[0]} your id is $id';
+  }
+
+  print(inner());
+}
